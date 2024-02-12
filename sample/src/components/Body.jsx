313c3e07a79e21317fdf589d1d0a0cbe30aeacc7
vaishnavi <vaishnavit582@gmail.com>
2024-02-12 19:34:37 +0530

    Hooks

--- sample/src/components/Body.jsx
+++ sample/src/components/Body.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RestaurantCard from './RestaurantCard';
 import resObj from '../utils/Mockdata';
 
 const Body = () => {
+  const [listOfRestau, setListOfRestau] = useState(resObj);
+
+  const handleFilter = () => {
+    const filteredList = resObj.filter((res) => res.data.avgRating > 4);
+    setListOfRestau(filteredList);
+  };
+
   return (
     <div className='body'>
-      <div className='search'></div>
+      <div className='filter'>
+        <button className='filter-btn' onClick={handleFilter}>
+          Top Rated Restaurants
+        </button>
+      </div>
+
       <div className='res-container'>
-        
-     {
-      resObj.map((restaurant)=><RestaurantCard key={restaurant.data.id} resData={restaurant}
-      />)
-     }
+        {listOfRestau.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
