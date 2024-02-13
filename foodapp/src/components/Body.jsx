347ff3c3023071464957267d4ce4546ac1cf1a69
vaishnavi <vaishnavit582@gmail.com>
2024-02-13 19:43:20 +0530

    API

--- foodapp/src/components/Body.jsx
+++ foodapp/src/components/Body.jsx
@@ -1,23 +1,66 @@
-import React, { useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+// import RestaurantCard from './RestaurantCard';
+
+
+// const Body = () => {
+//   const [listOfRestau, setListOfRestau] = useState([]);
+//   useEffect(()=>{fetchData();
+//   },[]);
+
+//   const fetchData= async()=>{
+//     const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&collection=94613&tags=layout_ux4&sortBy=&filters=&type=rcv2&offset=0&page_type=null");
+//     const jsonD=await data.json();
+//     console.log(jsonD);
+//     setListOfRestau(jsonD?.data?.cards[2]?.card);
+
+//   }
+ 
+ 
+
+//   return (
+//     <div className='body'>
+    
+
+//       <div className='res-container'>
+//         {listOfRestau.map((restaurant) => (
+//           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Body;
+
+
+
+
+import React, { useEffect, useState } from 'react';
 import RestaurantCard from './RestaurantCard';
-import resObj from '../utils/Mockdata';
 
 const Body = () => {
-  const [listOfRestau, setListOfRestau] = useState(resObj);
+  const [listOfRestau, setListOfRestau] = useState([]);
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const handleFilter = () => {
-    const filteredList = resObj.filter((res) => res.data.avgRating > 4);
-    setListOfRestau(filteredList);
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&collection=94613&tags=layout_ux4&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+      );
+      const jsonData = await data.json();
+
+      // Use optional chaining to access nested properties
+      setListOfRestau(jsonData?.data?.cards[2]?.card || []);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
 
   return (
     <div className='body'>
-      <div className='filter'>
-        <button className='filter-btn' onClick={handleFilter}>
-          Top Rated Restaurants
-        </button>
-      </div>
-
       <div className='res-container'>
         {listOfRestau.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
@@ -32,38 +75,6 @@ export default Body;
 
 
 
-// import React from 'react';
-// import RestaurantCard from './RestaurantCard';
-// import resObj from '../utils/Mockdata';
-// import { useState } from 'react';
-
-// const Body = () => {
-//   const [listOfRestau,setListOfRestau]=useState( )
-//   return (
-//     <div className='body'>
-//      <div className='filter'>
-//   <button className='filter-btn' onClick={() => {
-//     const filteredList = listOfRestau.filter((res) => {
-//       return res.data.avgRating >  4;
-//     });
-//     setListOfRestau(filteredList);
-//   }}>
-//     Top Rated Restaurants
-//   </button>
-// </div>
-
-//       <div className='res-container'>
-        
-//      {
-//       resObj.map((restaurant)=><RestaurantCard key={restaurant.data.id} resData={restaurant}
-//       />)
-//      }
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Body;
 
 
 
