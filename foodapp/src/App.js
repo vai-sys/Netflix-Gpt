a72db94fae08aefda263a5162e517a93586a4bf9
vaishnavi <vaishnavit582@gmail.com>
2024-02-21 11:26:38 +0530

    foodapp

--- foodapp/src/App.js
+++ foodapp/src/App.js
@@ -1,61 +1,41 @@
-
-
-
-
 import React from 'react';
-import {  BrowserRouter,Routes,Route } from 'react-router-dom';
+import { createBrowserRouter,  Outlet } from 'react-router-dom';
 import './App.css';
-
+import Header from './components/Header';
 import Body from './components/Body';
 import About from './components/About';
 import Error from './components/Error';
 import Contact from './components/Contact';
-import Home from './components/Home';
-const App = () => {
-  return <BrowserRouter>
-     
-     <Routes>
-     <Route path='/' element={<Home/>}>
-      <Route path='body' element={<Body/>}/>
-     <Route path='about' element={<About/>}/>
-     <Route path='contact' element={<Contact/>}/>
-     <Route path='*' element={<Error/>}/>
-     </Route>
-     </Routes>
-     
-     </BrowserRouter>
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Outlet />
+    </div>
+  );
 }
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Body />,
+      },
+      {
+        path: 'About',
+        element: <About />,
+      },
+      {
+        path: 'Contact',
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
-// const appRouter = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     children:[
-//       {path:"/Body",
-//     element:<Body/>},
-//       {
-//         path: "/About",
-//         element: <About />,
-//       },
-//       {
-//         path:"/Contact",
-//         element:<Contact/>,
-//       },
-//     ],
-//     errorElement: <Error />,
-//   },
- 
-// ]);
-
-// const root = createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <RouterProvider router={appRouter}>
-//       <App />
-//     </RouterProvider>
-//   </React.StrictMode>
-// );
-
-
+export { App,router};
