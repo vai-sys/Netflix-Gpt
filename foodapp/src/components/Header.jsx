0eac5a50b53813f220fec393c89de20286a449b3
vaishnavi <vaishnavit582@gmail.com>
2024-02-25 13:57:45 +0530

    Styling

--- foodapp/src/components/Header.jsx
+++ foodapp/src/components/Header.jsx
@@ -1,23 +1,24 @@
+// Header.jsx
 import React, { useState } from 'react';
 import { GrCart } from 'react-icons/gr';
 import { NavLink } from 'react-router-dom';
-import useOnlineStatus from '../utils/useOnlineStatus'
+import useOnlineStatus from '../utils/useOnlineStatus';
 
 const Header = () => {
   const [loginname, setloginname] = useState('Login');
-  const onlineStatus=useOnlineStatus();
+  const onlineStatus = useOnlineStatus();
 
   return (
-    <div className='header'>
+    <div className='container'>
       <div className='logo-container'>
         <img className='logo' alt='logo' src='https://seeklogo.com/images/F/food-logo-59E5A73AFD-seeklogo.com.png'></img>
       </div>
       <div className='nav-items'>
         <ul>
-          <li>Network Status:{onlineStatus ? "✅" :"🔴"}</li>
-          <li><NavLink to="/" style={{textDecoration:"none",color:"black"}}>Home</NavLink></li>
-          <li><NavLink to="/About"  style={{textDecoration:"none",color:"black"}}>About Us</NavLink></li>
-          <li><NavLink to="/Contact"  style={{textDecoration:"none",color:"black"}}>Contact Us</NavLink></li>
+          <li>Network Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li><NavLink to="/" className='nav-link' activeClassName='active'>Home</NavLink></li>
+          <li><NavLink to="/About" className='nav-link' activeClassName='active'>About Us</NavLink></li>
+          <li><NavLink to="/Contact" className='nav-link' activeClassName='active'>Contact Us</NavLink></li>
           <li><GrCart className='cart' /></li>
           <button className='login' onClick={() => {
             loginname === 'Login' ? setloginname('Logout') : setloginname('Login');
@@ -29,5 +30,9 @@ const Header = () => {
 };
 
 export default Header;
+
+
+
+
 
 
